ac76e2f8626e2278cc14800cef71797bdb5c6765
unknown <shamsshsh@code.edu.az>
2023-02-17 01:06:42 +0400

    ConsoleAppP138Sh

---
+++ ConsoleAppP138Sh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppP138Sh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
